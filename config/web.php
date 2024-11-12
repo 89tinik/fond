65f88ea8c9320a30bce7bb51c8ff652e9b53f5ab
fonnd server <fond@pushkin.ru>
2024-11-12 21:25:18 +0300

    server friendly URL

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'icyIBOm_OurjIwRE9biHRZGNLJhtCp2B',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -42,14 +43,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
